0d60cc9856dc759fa492c7acedd3c0bc63a0af6d
Fb1234566 <58706918+Fb1234566@users.noreply.github.com>
2023-09-15 22:11:44 +0200

    Frontend (#5)
    * aggiunta data e ora di creazione e di aggirnamento
    
    * creata REST api per modelli Person e Character
    
    * modified urls name
    
    * modificati modelli
    
    * api movies aggiunta quai completamente
    
    * tutte api implementate quasi corretamente
    
    * modificati modelli review e movie
    
    * aggiunto swagger ui
    
    * aggiunto index page
    
    * modificato index page
    
    * sistemato problema visualizzazione attori
    
    * modificato leggermente modello Review
    
    * modificato leggermente api Reviews
    
    * modificato leggermente api Reviews

--- mymdb/reviews/models.py
+++ mymdb/reviews/models.py
@@ -10,10 +10,10 @@ class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, default=None, related_name="reviews")
     #tags = GenericRelation(Movie, related_query_name="reviews")
     #content_type = models.ForeignKey(ContentType,
-    #   on_delete=models.https://github.com/Fb1234566/mymdb-challenge/pull/4/conflict?name=mymdb%252Freviews%252Fapi%252Fviews.py&base_oid=d45e075fb5b04a76617c2636e3f28eea83cde2ba&head_oid=ac249ad0fef0793b645edcbe531d8a38b3ae3f57DO_NOTHING,
     #   related_name="reviews"
     #
     #bject_id = models.PositiveIntegerField()
     #bj = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(default=datetime.now)
     updated_at = models.DateTimeField(default=datetime.now)
+
