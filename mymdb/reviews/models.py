de37b54df7eeb83df18ea65c995ab89dae8d71e3
Fb1234566 <58706918+Fb1234566@users.noreply.github.com>
2023-09-15 22:00:33 +0200

    Models (#2)
    * aggiunta data e ora di creazione e di aggirnamento
    
    * creata REST api per modelli Person e Character
    
    * modified urls name
    
    * modificati modelli
    
    * api movies aggiunta quai completamente
    
    * tutte api implementate quasi corretamente
    
    * modificati modelli review e movie
    
    * aggiunto swagger ui
    
    * aggiunto index page
    
    * modificato index page

--- mymdb/reviews/models.py
+++ mymdb/reviews/models.py
@@ -7,7 +7,6 @@ from django.contrib.contenttypes.models import ContentType
 
 class Review(models.Model):
     review = models.CharField(max_length=20000, default='')
-
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, default=None, related_name="reviews")
     #tags = GenericRelation(Movie, related_query_name="reviews")
     #content_type = models.ForeignKey(ContentType,
@@ -17,7 +16,5 @@ class Review(models.Model):
     #bject_id = models.PositiveIntegerField()
     #bj = GenericForeignKey('content_type', 'object_id')
     created_at = models.DateTimeField(default=datetime.now)
-
     updated_at = models.DateTimeField(default=datetime.now)
-    
 
